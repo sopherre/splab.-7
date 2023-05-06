065c652db39a73c7ca121a78b251f3f30636b0f3
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-05-06 13:39:23 +0900

    testUtilsにuser追加

--- src/components/contextMenuContainer/__tests__/contextMenuContainer.test.tsx
+++ src/components/contextMenuContainer/__tests__/contextMenuContainer.test.tsx
@@ -1,13 +1,12 @@
-import { render, screen } from "@/test/testUtils";
+import { render, screen, user } from "@/test/testUtils";
 import { ContextMenuContainer } from "../contextMenuContainer";
-import userEvent from "@testing-library/user-event";
 
 describe("ContextMenuContainerコンポーネントのテスト", () => {
   it("コンテナ内のテキストを右クリックするとメニューが表示される", async () => {
     render(<ContextMenuContainer />);
     const text = screen.getByText("Lorem", { exact: false });
 
-    await userEvent.pointer({ keys: "[MouseRight>]", target: text });
+    await user.pointer({ keys: "[MouseRight>]", target: text });
 
     const menu = screen.getByRole("menu");
     expect(menu).toBeInTheDocument();
--- src/test/testUtils.tsx
+++ src/test/testUtils.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "@/providers/themeProvider";
 import { render, RenderOptions } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 const AllTheProviders: React.FC<{ children: React.ReactNode }> = ({
   children,
@@ -10,5 +11,7 @@ const customRender = (
   options?: Omit<RenderOptions, "wrapper">
 ) => render(ui, { wrapper: AllTheProviders, ...options });
 
+const user = userEvent.setup();
+
 export * from "@testing-library/react";
-export { customRender as render };
+export { customRender as render, user };
