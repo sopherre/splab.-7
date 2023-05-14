6c70929be3453fad2f79810e5076aa1f8eb44e68
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-05-14 19:37:07 +0900

    DrawerContainerのロジック追加

--- src/components/drawerContainer/drawerContainer.tsx
+++ src/components/drawerContainer/drawerContainer.tsx
@@ -1,38 +1,64 @@
 import { Button, Drawer, IconButton, Typography, styled } from "@mui/material";
 import { memo, useCallback, useState } from "react";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
-import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 
-const StyledDrawer = styled(Drawer)(({ theme }) => ({
+const StyledDrawer = styled(Drawer)<{ open: boolean }>(({ theme, open }) => ({
   "& .MuiDrawer-paper": {
     padding: theme.spacing(3),
     justifyContent: "space-between",
+    overflow: "visible",
+    visibility: "visible!important",
   },
+}));
+
+const StyledIconButton = styled(IconButton)(({ theme }) => ({
+  position: "absolute",
+  zIndex: 100,
+  left: -30,
+  backgroundColor: theme.palette.secondary.main,
 }));
 
 export const DrawerContainer: React.FC = memo(() => {
   const [open, setOpen] = useState(false);
 
-  const handleClick = useCallback(() => setOpen(true), []);
+  const openDrawer = useCallback(() => setOpen(true), []);
 
-  const handleClose = useCallback(() => setOpen(false), []);
+  const closeDrawer = useCallback(() => setOpen(false), []);
+
+  const handleIconButtonClick = useCallback(() => {
+    if (open) {
+      closeDrawer();
+      return;
+    }
+    openDrawer();
+  }, [closeDrawer, open, openDrawer]);
 
   return (
     <>
       <div>
         <Typography>ドロワーのテスト</Typography>
-        <Button variant="contained" onClick={handleClick}>
+        <Button variant="contained" onClick={openDrawer}>
           開く
         </Button>
       </div>
 
-      <StyledDrawer open={open} variant="persistent" anchor="right">
-        <IconButton>
-          {open ? <ArrowRightAltIcon /> : <ArrowLeftIcon />}
-        </IconButton>
+      <StyledDrawer
+        data-testid="Drawer"
+        open={open}
+        variant="persistent"
+        anchor="right"
+      >
+        <StyledIconButton onClick={handleIconButtonClick}>
+          {open ? (
+            <ArrowRightAltIcon titleAccess="arrowRightIcon" />
+          ) : (
+            <KeyboardBackspaceIcon titleAccess="arrowLeftIcon" />
+          )}
+        </StyledIconButton>
 
         <Typography>ドロワー</Typography>
-        <Button variant="contained" onClick={handleClose}>
+        <Button variant="contained" onClick={closeDrawer}>
           閉じる
         </Button>
       </StyledDrawer>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,6 @@ export default function Home() {
         <TooltipContainer />
         <UploadContainer />
         <InputContainer />
-        <DrawerContainer />
       </Main>
     </>
   );
